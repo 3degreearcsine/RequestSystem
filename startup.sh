05cd2f33ffdeaee428f042a74899a721057c07dc
sawdisha <87311587+sawdisha@users.noreply.github.com>
2022-04-06 03:57:59 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8443 --timeout 600
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8443 --timeout 600
