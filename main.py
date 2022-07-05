27c82394f81ba62278438ca9b3becf3b1a9e99b8
sawdisha <dishasawant1672@gmail.com>
2022-07-05 10:27:53 +0530

    Template rendering updated

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Request
 from dbase import models
 from routes import rec_request, user, doubt_clearing_request, auth, admin, tutor
 from dbase.database import engine
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -14,39 +15,9 @@ app.include_router(doubt_clearing_request.router)
 app.include_router(admin.router)
 app.include_router(tutor.router)
 
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/",status_code=status.HTTP_200_OK, response_class=HTMLResponse)
-def home():
-    return """
-    <html>
-        <head>
-            <title>Request System</title>
-            <style>
-            .button{
-            border: none;
-            color: white;
-            padding: 15px 32px;
-            text-align: center;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-            }
-            .button1{background-color: #4CAf50;}
-            .button2{background-color: #008CBA;}
-            </style>            
-        </head>
-        <body>
-            <h1>Welcome to the request System</h1>
-            <h2> Please Register if your are a new user or Login if you are existing user</h2>
-            <button class="button button1" onclick="location.href='https://reqsys.azurewebsites.net/login';">Login</button><br>
-            <button class="button button2" onclick="location.href='https://reqsys.azurewebsites.net/register';">Register</button>
-        </body>
-    <html>
-    """
-
-
-
-
-
-
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
