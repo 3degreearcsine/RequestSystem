9111bccf75a4cb480c856e684733a9338683b115
sawdisha <dishasawant1672@gmail.com>
2022-08-04 10:54:45 +0530

    middleware time_haeder added

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, status, Request, Response
+import time
+from fastapi import FastAPI, status, Request, Response, HTTPException
 from app.dbase import models
 from app.routes import admin, auth, user, doubt_clearing_request, rec_request, tutor
 from app.dbase.database import engine
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from app import exceptions
+from app import exceptions, oauth2
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -27,6 +28,15 @@ def forbidden_exception_handler():
     error = "Access Forbidden"
     Response.status_code = status.HTTP_403_FORBIDDEN
     return error
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 @app.get("/",status_code=status.HTTP_200_OK, response_class=HTMLResponse)
